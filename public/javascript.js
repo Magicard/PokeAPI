a15a243b1cd03d926addea1655130a1272c31e2c
Teodor Sava <mice.graff@gmail.com>
2019-05-29 22:37:05 +0100

    Added more examples and made burger button work when in mobile

--- public/javascript.js
+++ public/javascript.js
@@ -1,5 +1,5 @@
 function changeImage(){
     var image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+    
     document.getElementById("pokepic").src= "1.png";
-
 }
